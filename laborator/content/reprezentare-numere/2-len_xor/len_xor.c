78ce1e82fd23f789aa0da6684d1a5e1d3dc7d73e
root <root@DESKTOP-2QHQ3TI>
2024-03-14 22:07:10 +0200

    lab1 ex2
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -13,6 +13,11 @@ int my_strlen(const char *str)
 	 * Remove this cast when implementing the function.
 	 */
 	(void) str;
+    int l = 0;
+    while (*str++) {
+        l++;
+    }
+    return l;
 
 	return -1;
 }
@@ -21,12 +26,26 @@ void equality_check(const char *str)
 {
 	/* TODO */
 	(void) str;
+	
+    int l = my_strlen(str);
+    printf("Lungime = %d\n", l);
+    
+    for (int i = 0; i < l; i++) {
+        const char *adresa = str + ((i + (1 << i)) % l);
+        printf("Adresa %c: %p\n", *(str + i), adresa);
+    }
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
+	const char *s1 = "aac";
+    printf("%s: \n", s1);
+    equality_check(s1);
 
+    const char *s2 = "ababababacccbacbacbacbacbabc";
+    printf("%s:\n", s2);
+    equality_check(s2);
 	return 0;
 }
 
