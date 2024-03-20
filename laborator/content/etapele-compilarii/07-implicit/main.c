1d44184610aa082cd7295f5ea92e1e5d8cbb8418
root <root@DESKTOP-2QHQ3TI>
2024-03-20 19:39:35 +0200

    Ex 7 editare fisier main.c
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/etapele-compilarii/07-implicit/main.c
+++ laborator/content/etapele-compilarii/07-implicit/main.c
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <stdio.h>
+extern unsigned long long compute_double(unsigned long long val);
 
 int main() {
 	unsigned long long val = 0xFFFFFFFF;
-	unsigned long long double_val;
+    unsigned long long double_val;
 
-	double_val = compute_double(val);
+    double_val = compute_double(val);
 
-	printf("2 * %llx = %llx\n", val, double_val);
+    printf("2 * %lld = %lld\n", val, double_val);
 
-	return 0;
+    return 0;
 }
+
+
+
