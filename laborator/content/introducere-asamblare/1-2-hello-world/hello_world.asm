7323a3d29e0a60799b6b42b16480d7de91f1383c
root <root@DESKTOP-2QHQ3TI>
2024-04-03 18:35:54 +0300

    Lab 5 ex 1-2
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
+++ laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
@@ -2,13 +2,14 @@
 
 section .data
     myString: db "Hello, World!", 0
+    goodbye: db "Goodbye, World!", 0
 
 section .text
     global main
     extern printf
 
 main:
-    mov ecx, 6                      ; N = valoarea registrului ecx
+    mov ecx, 6                      ; N = 6 valoarea registrului ecx
     mov eax, 1
     mov ebx, 1
     cmp eax, ebx
@@ -18,6 +19,9 @@ main:
 print:
     PRINTF32 `%s\n\x0`, myString
                                     ; TODO2.2: afisati "Hello, World!" de N ori
+    dec ecx
+    cmp ecx, 0
+    jg print
                                     ; TODO2.1: afisati "Goodbye, World!"
-
+    PRINTF32 `%s\n\x0`, goodbye
     ret
