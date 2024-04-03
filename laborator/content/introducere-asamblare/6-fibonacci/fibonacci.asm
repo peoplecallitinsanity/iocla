a2801aa0cfc2f5b9534ea53410ea1c57956686a3
root <root@DESKTOP-2QHQ3TI>
2024-04-03 19:26:07 +0300

    Lab 05 ex 6
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -5,8 +5,20 @@ section .text
     extern printf
 
 main:
-    mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
-
+    mov ecx, 7       ; vrem sa aflam al N-lea numar; N = 7
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
+    mov eax, 0
+    mov ebx, 1
 
+fibonacci:
+    dec ecx
+    test ecx, ecx
+    je print
+    add eax, ebx
+    xchg eax, ebx
+    jmp fibonacci
+
+print:
+    PRINTF32 `%d\n\x0`, ebx
+    xor eax, eax
     ret
