5b2de78c0fadb46931fbc89d855ac256d0f5dbcc
root <root@DESKTOP-2QHQ3TI>
2024-04-10 19:24:03 +0300

    Lab 06 ex 3-4
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

---
+++ laborator/content/rolul-registrelor-adresare/3-4-sum-n/sum-n-square.asm
@@ -0,0 +1,27 @@
+%include "../utils/printf32.asm"
+
+section .data
+    num dd 100
+
+section .text
+extern printf
+global main
+main:
+    push ebp
+    mov ebp, esp
+
+    mov ecx, [num]     ; Use ecx as counter for computing the sum.
+    xor eax, eax       ; Use eax to store the sum. Start from 0.
+    xor ebx, ebx
+
+add_to_sum:
+    mov eax, ecx
+    mul eax
+    add ebx, eax
+    loop add_to_sum    ; Decrement ecx. If not zero, add it to sum.
+
+    mov ecx, [num]
+    PRINTF32 `Sum(%u): %u\n\x0`, ecx, ebx
+
+    leave
+    ret
