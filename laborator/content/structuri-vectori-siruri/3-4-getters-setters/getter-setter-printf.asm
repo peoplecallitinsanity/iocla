d283f7fe9b3b22ae67e6e02417292192869cc2a8
root <root@DESKTOP-2QHQ3TI>
2024-04-17 18:55:06 +0300

    Lab 07 ex 3-4
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
+++ laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
@@ -18,6 +18,8 @@ section .data
     new_char db 'b'
     new_string db 'Are you sure?', 0
 
+
+
 section .text
 extern printf
 global main
@@ -30,19 +32,51 @@ main:
     ; print all three values (int_x, char_y, string_s) from simple_obj
     ; Hint: use "lea reg, [base + offset]" to save the result of
     ; "base + offset" into register "reg"
+    xor ebx, ebx
+    mov ebx, [sample_obj + int_x]
+    PRINTF32 `int_x: %d\n\x0`, ebx
+    xor ebx, ebx
+    mov ebx, [sample_obj + char_y]
+    PRINTF32 `char_y: %c\n\x0`, ebx
+
+    lea eax, [sample_obj + string_s]
+    PRINTF32 `string_s: %s\n\x0`, eax
 
     ; TODO
     ; write the equivalent of "simple_obj->int_x = new_int"
+    mov eax, [new_int]
+    mov [sample_obj + int_x], eax
 
     ; TODO
     ; write the equivalent of "simple_obj->char_y = new_char"
-
+    mov bl, [new_char]
+    mov [sample_obj + char_y], bl
     ; TODO
     ; write the equivalent of "strcpy(simple_obj->string_s, new_string)"
+   
+
+    xor edx, edx
+loop:
+    mov al, [new_string + edx]
+    mov [sample_obj + string_s + edx], al
+    inc edx
+    cmp al, 0
+    jnz loop
+
+
 
     ; TODO
     ; print all three values again to validate the results of the
     ; three set operations above
+        xor ebx, ebx
+    mov ebx, [sample_obj + int_x]
+    PRINTF32 `int_x: %d\n\x0`, ebx
+    xor ebx, ebx
+    mov ebx, [sample_obj + char_y]
+    PRINTF32 `char_y: %c\n\x0`, ebx
+
+    lea eax, [sample_obj + string_s]
+    PRINTF32 `string_s: %s\n\x0`, eax
 
     xor eax, eax
     leave
