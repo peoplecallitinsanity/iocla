4b855fcddd8852c3c20c1822c5ff127caa6825cb
root <root@DESKTOP-2QHQ3TI>
2024-04-24 19:07:40 +0300

    Lab 08 Ex2
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -12,10 +12,19 @@ section .text
 extern printf
 global main
 main:
-
+    push ARRAY_LEN
+    pop ecx
     ; TODO push the elements of the array on the stack
+pushh:
+    push dword [input + 4 * (ecx - 1)]
+    loop pushh
     ; TODO retrieve the elements (pop) from the stack into the output array
 
+    push ARRAY_LEN
+    pop ecx
+popp:
+    pop dword [output + 4 * (ecx-1)]
+    loop popp
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
 print_array:
