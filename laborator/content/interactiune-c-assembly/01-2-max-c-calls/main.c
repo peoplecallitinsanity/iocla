409a9371c48c040dcfc1d0188c6f056ab85be3b3
root <root@DESKTOP-2QHQ3TI>
2024-05-15 18:32:30 +0300

    Lab 10 ex 1-2
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/interactiune-c-assembly/01-2-max-c-calls/main.c
+++ laborator/content/interactiune-c-assembly/01-2-max-c-calls/main.c
@@ -2,16 +2,15 @@
 
 #include <stdio.h>
 
-unsigned int get_max(unsigned int *arr, unsigned int len);
+unsigned int get_max(unsigned int *arr, unsigned int len, unsigned int* pos);
 
 int main(void)
 {
 	unsigned int arr[] = { 19, 7, 129, 87, 54, 218, 67, 12, 19, 99 };
-	unsigned int max;
+	unsigned int pos;
+	get_max(arr, 10,&pos);
 
-	max = get_max(arr, 10);
-
-	printf("max: %u\n", max);
+	printf("max: %u\n", arr[pos]);
 
 	return 0;
 }
